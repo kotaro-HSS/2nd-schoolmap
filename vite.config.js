9f8a8b9e53adb55abf5704b803b0663ead444fc1
Kotaro Matsumoto <00890001313573eq@gl.pen-kanagawa.ed.jp>
2025-12-28 22:27:25 +0900

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/2nd-schoolmap/',
   plugins: [react()],
-  base: "/1st-pre-my-map-app/",
-});
-
-
-
-
+})
